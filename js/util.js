38a986706d69794e18cf27cf45dab89e2e964674
573381010@qq.com <573381010@qq.com>
2021-01-23 19:24:57 +0800

    Three.js创建第一个三维场景

---
+++ js/util.js
@@ -0,0 +1,22 @@
+function initStats(type) {
+  var panelType = (typeof type !== 'undefined' && type) && (!isNaN(type)) ? parseInt(type) : 0;
+  var stats = new Stats();
+
+  stats.showPanel(panelType); // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.body.appendChild(stats.dom);
+
+  return stats;
+}
+function initTrackballControls(camera, renderer) {
+  var trackballControls = new THREE.TrackballControls(camera, renderer.domElement);
+  trackballControls.rotateSpeed = 1.0;
+  trackballControls.zoomSpeed = 1.2;
+  trackballControls.panSpeed = 0.8;
+  trackballControls.noZoom = false;
+  trackballControls.noPan = false;
+  trackballControls.staticMoving = true;
+  trackballControls.dynamicDampingFactor = 0.3;
+  trackballControls.keys = [65, 83, 68];
+
+  return trackballControls;
+}
